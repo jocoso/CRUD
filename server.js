d382d2b3cfa176a123dc1ec0917613973a062a7d
jocoso <joshua.collado022@gmail.com>
2023-12-02 08:37:15 -0500

    Created modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 dotenv.config( { path: 'config.env'} );
@@ -8,6 +10,18 @@ const PORT = process.env.PORT || 8080;
 
 // Requests
 app.use(morgan("tiny"));
+
+// Parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Views
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "view/ejs"));
+
+// Assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 
 app.get('/', (req, res) => {
     res.send("Crud Application");
